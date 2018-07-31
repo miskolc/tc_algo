754bc13744bdd3a9605dad26ccc8f86446d544c5
Abhishek Pahwa <apahwa014@gmail.com>
2018-07-31 19:15:52 +0530

    added TODO

--- indicators.py
+++ indicators.py
@@ -7,10 +7,21 @@ import numpy
 _logger = logging.getLogger('indicator')
 
 
+# TODO: List of Indicators:
+# TODO: 1. Momentum - RSI, Stochastic Oscillator
+# TODO: 2. Trend - SMA,EMA,MACD
+# TODO: 3. Volatility - Bollinger Bands
+# TODO: 4. Support and Resistance - Pivot Points
+
+
 # Get info for the required function in ta-lib
-def indicator_info(indicator=str()):
-    var = Function(indicator).info
-    _logger.info(var)
+def indicator_info(indicator=""):
+    if indicator is "":
+        _logger.warning('Enter valid indicator name. For e.g. indicator_info("EMA")')
+        _logger.info("For complete list visit http://mrjbq7.github.io/ta-lib/funcs.html")
+    else:
+        info = Function(indicator).info
+        _logger.info(info)
 
 
 # Simple moving average. Default period = 30
@@ -20,6 +31,41 @@ def remove_nan(result):
     return result
 
 
+# Relative Strength Index. Default period = 14
+def rsi(array=None, period=14):
+    result = []
+    if (array is None) | (array == []):
+        _logger.warning("Invalid Input")
+    else:
+        if period < 1:
+            _logger.warning("Period should be greater than 0")
+        else:
+            if len(array) < period:
+                _logger.warning("Period greater than length of input. Unexpected behaviour may occur")
+            result = talib.RSI(array, period)
+            result = remove_nan(result)
+        _logger.debug('RSI output: %s' % result)
+    return result
+
+
+# Stochastic Oscillator. 
+def stoch(high=None, low=None, close=None, fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3,
+          slowd_matype=0):
+    result = []
+    if (high is None) | (high == []):
+        _logger.warning("Invalid Input")
+    else:
+        if fastk_period < 1:
+            _logger.warning("Period should be greater than 0")
+        else:
+            if len(high) < fastk_period:
+                _logger.warning("Period greater than length of input. Unexpected behaviour may occur")
+            result = talib.STOCH(high, low, close, fastk_period, slowk_period, slowk_matype, slowd_period, slowd_matype)
+        _logger.debug('STOCH output: %s' % result)
+    return result
+
+
+# Simple Moving Average. Default period = 30
 def sma(array=None, period=30):
     result = numpy.ndarray
     if array is None:
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import logging
+import tkinter
 import numpy
 
 import indicators
 
 
+def onClick():
+    print("Button pressed")
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     close = numpy.random.random(105) * 20
-    indicators.sma(close, 25)
+    # indicators.sma(close, 25)
     # indicators.ema(close, 20)
-    # indicators.indicator_info("ema")
-
-
+    # indicators.indicator_info("RSI")
+    indicators.rsi(close)
