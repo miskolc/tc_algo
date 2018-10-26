bc0a8e7a49d6fe127c47ee0435c35fcf7a55d480
Abhishek Pahwa <apahwa014@gmail.com>
2018-10-26 12:24:46 +0530

    Update: Payoff charts done

--- options/option_greeks.py
+++ options/option_greeks.py
@@ -63,11 +63,12 @@ def get_greeks(spot_price, strike_price, expiry_date: date, option_type: str, op
 
 
 def get_option_greeks(spot_price, strike_price, expiry_date: date, option_type: str, option_price: float,
-                      calculation_date: date = None, implied_vol: float = None):
+                      calculation_date: date = None, volatility: float = None):
     maturity_date = Date(expiry_date.day, expiry_date.month, expiry_date.year)
     # volatility = 0.0  # the historical vols for a year
     # print(spot_price, strike_price, expiry_date, option_type, option_price, calculation_date)
 
+    volatility = volatility / 100
     option = Option.Call
     if option_type == Keys.call:
         option = Option.Call
@@ -86,15 +87,17 @@ def get_option_greeks(spot_price, strike_price, expiry_date: date, option_type: 
 
     spot_handle = QuoteHandle(SimpleQuote(spot_price))
     flat_ts = YieldTermStructureHandle(FlatForward(calculation_date, risk_free_rate, day_count))
-    flat_vol_ts = BlackVolTermStructureHandle(BlackConstantVol(calculation_date, calendar, implied_vol, day_count))
+    flat_vol_ts = BlackVolTermStructureHandle(BlackConstantVol(calculation_date, calendar, volatility, day_count))
     bs_process = BlackScholesProcess(spot_handle, flat_ts, flat_vol_ts)
 
     european_option.setPricingEngine(AnalyticEuropeanEngine(bs_process))
 
+    # price = european_option.NPV()
     delta = european_option.delta()
     gamma = european_option.gamma()
     theta = european_option.thetaPerDay()
     vega = european_option.vega()
     rho = european_option.rho()
 
+    # print(price, delta, )
     return delta, gamma, theta, vega, rho
