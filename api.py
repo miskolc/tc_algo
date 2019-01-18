245045d4cc5ac73adb52f58121fb79505ccfa21b
Abhishek Pahwa <apahwa014@gmail.com>
2019-01-18 16:58:58 +0530

    Add: Data Sub-Package and Quandl Addition

--- api.py
+++ api.py
@@ -5,7 +5,6 @@ For complete list of symbols visit: https://www.quandl.com/data/NSE-National-Sto
 from model import Symbol
 
 quandl_api_key = "y4jybLNT_iLmo_jJMfae"
-link = "https://www.quandl.com/api/v3/datasets/NSE/CNX_NIFTY.json?api_key=y4jybLNT_iLmo_jJMfae"
 
 min_date = "03-07-1990"
 
